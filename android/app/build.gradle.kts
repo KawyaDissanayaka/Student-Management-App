01330a4f68bf24c7a7a79eb55366d159abcab4cf
Kawya Dissanayaka <dissanayakadank@students.nsbm.ac.lk>
2026-08-18 19:18:22 +0530

    initialize Android build configuration, manifest permissions, and web hosting files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,11 +18,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.student.app.student_management_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -30,7 +27,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
